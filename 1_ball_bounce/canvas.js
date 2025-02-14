5beb958350911780dd0893906247a11de772cb77
Lee Presswood <leepresswood@gmail.com>
2025-02-14 16:00:43 -0700

    Added Circle class with its own draw method.

--- 1_ball_bounce/canvas.js
+++ 1_ball_bounce/canvas.js
@@ -7,30 +7,48 @@ canvas.height = window.innerHeight;
 //Get the manipulable 2D context of the canvas.
 const ctx = canvas.getContext("2d");
 
-const radius = 30;
-let x = radius;
-let y = radius;
+class Circle {
+    speed = 6;
+    radius = 30;
 
-let dx = 1;
-let dy = 1;
+    constructor() {
+        this.x = Math.random() * (innerWidth - this.radius * 2) + this.radius;
+        this.y = Math.random() * (innerHeight - this.radius * 2) + this.radius;
+        this.dx = this.speed * (Math.random() > 0.5 ? 1 : -1);
+        this.dy = this.speed * (Math.random() > 0.5 ? 1 : -1);
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.strokeStyle = "blue";
+        ctx.fillStyle = "rgba(0, 0, 255, 0.2)";
+        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
+        ctx.stroke();
+        ctx.fill();
+    }
+
+    update() {
+        if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+            this.dx = -this.dx;
+        }
+
+        if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+            this.dy = -this.dy;
+        }
+
+        this.x += this.dx;
+        this.y += this.dy;
+
+        this.draw();
+    }
+}
+
+const circles = Array.from({ length: 10 }, (v, k) => new Circle());
 
 function animate() {
     requestAnimationFrame(animate);
     ctx.clearRect(0, 0, innerWidth, innerHeight);
-    ctx.beginPath();
-    ctx.strokeStyle = "blue";
-    ctx.fillStyle = "rgba(0, 0, 255, 0.2)";
-    ctx.arc((x += dx), (y += dy), radius, 0, 2 * Math.PI, false);
-    ctx.stroke();
-    ctx.fill();
-
-    if (x + radius > innerWidth || x - radius < 0) {
-        dx = -dx;
-    }
-
-    if (y + radius > innerHeight || y - radius < 0) {
-        dy = -dy;
-    }
+    circles.forEach((circle) => circle.update());
 }
 
 animate();
